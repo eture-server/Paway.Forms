ed867237e3643ea1ca9966287c5e88a8b1635b77
tinn <tinn@c1914438-a9fc-4b41-8dc5-b03b8268c31d>
2014-10-13 01:48:06 +0000

    Mobot.Image 混淆
    git-svn-id: https://tinn-pc:8443/svn/Common/Paway.Forms@146 c1914438-a9fc-4b41-8dc5-b03b8268c31d

--- Mobot.Imaging.cmd
+++ Mobot.Imaging.cmd
@@ -1,5 +1,5 @@
 @echo off
-@set POSTBUILD_HOME="C:\Program Files\Xenocode\Postbuild 2010 for .NET\XBuild.exe"
+@set POSTBUILD_HOME="C:\Program Files (x86)\Xenocode\Postbuild 2010 for .NET\XBuild.exe"
 
 @echo off
 @echo 发布目录更新脚本
@@ -7,12 +7,11 @@
 @set BIN=bin\Release
 @set OUT=bin\Fuscated
 
-rd /S /Q "%OUT%"
 @echo.
 @echo ------------------------------------------------------------
-@echo 混淆程序集 Paway.Forms...
+@echo 混淆程序集 Mobot.Imaging...
 @echo ------------------------------------------------------------
-%POSTBUILD_HOME% Paway.Forms.postbuild /allstrings /o "%OUT%"
+%POSTBUILD_HOME% Mobot.Imaging.postbuild /allstrings /o "%OUT%"
 
 @echo.
 @echo ==============================================================================
