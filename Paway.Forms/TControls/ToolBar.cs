fe68c2b4c7ce9041b23f1ef973bd34b830ce4cdb
tinn <tinn@c1914438-a9fc-4b41-8dc5-b03b8268c31d>
2017-09-06 01:12:17 +0000

    自动最小宽度
    git-svn-id: https://tinn-pc:8443/svn/Common/Paway.Forms@428 c1914438-a9fc-4b41-8dc5-b03b8268c31d

--- Paway.Forms/TControls/ToolBar.cs
+++ Paway.Forms/TControls/ToolBar.cs
@@ -1059,6 +1059,7 @@ namespace Paway.Forms
             if (_iAutoWidth)
             {
                 if (width > _itemSize.Width) width = _itemSize.Width;
+                if (width < _itemSize.Width / 2) width = _itemSize.Width / 2;
                 item.Rectangle = new Rectangle(item.Rectangle.X, item.Rectangle.Y, width, item.Rectangle.Height);
             }
             width = size.Width.ToInt();
