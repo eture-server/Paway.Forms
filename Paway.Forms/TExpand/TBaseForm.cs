67cd4cd6771cf621a7f363a282f7a345d7138447
tinn <tinn@c1914438-a9fc-4b41-8dc5-b03b8268c31d>
2019-01-10 09:25:03 +0000

    补充：基础功能控件分家
    git-svn-id: https://tinn-pc:8443/svn/Common/Paway.Forms@549 c1914438-a9fc-4b41-8dc5-b03b8268c31d

--- Paway.Forms/TExpand/TBaseForm.cs
+++ Paway.Forms/TExpand/TBaseForm.cs
@@ -1,28 +1,18 @@
-﻿using Paway.Helper;
-using Paway.Win32;
-using System;
-using System.Drawing;
-using System.Windows.Forms;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace Paway.Forms
 {
     /// <summary>
     ///     窗体示例
     /// </summary>
-    public partial class TBaseForm : QQForm
+    public class TBaseForm : IBaseForm
     {
         /// <summary>
-        /// 双Esc关闭
         /// </summary>
-        protected bool iExit;
-        /// <summary>
-        /// 响应回车到下一控件
-        /// </summary>
-        protected bool IEnter = true;
-        /// <summary>
-        /// 响应回车点击事件
-        /// </summary>
-        protected bool IEnterClick = false;
+        protected TControl panel1;
 
         /// <summary>
         /// 构造
@@ -30,180 +20,48 @@ namespace Paway.Forms
         public TBaseForm()
         {
             InitializeComponent();
-            this.TextShow = string.Empty;
-            this.TMouseMove(this.lbTitle);
             this.TMouseMove(this.panel1);
-            lbTitle.TextChanged += LbTitle_TextChanged;
         }
 
-        #region 其它
-        /// <summary>
-        ///     关闭时激发父窗体
-        /// </summary>
-        /// <param name="e"></param>
-        protected override void OnFormClosing(FormClosingEventArgs e)
+        private void InitializeComponent()
         {
-            if (Owner != null)
-            {
-                Owner.Activate();
-            }
-            base.OnFormClosing(e);
-        }
-        /// <summary>
-        /// 调整Title位置
-        /// </summary>
-        protected override void OnResize(EventArgs e)
-        {
-            base.OnResize(e);
-            if (lbTitle != null)
-            {
-                this.lbTitle.Location = new Point((this.Width - lbTitle.Width) / 2, 32);
-            }
-        }
-        private void LbTitle_TextChanged(object sender, EventArgs e)
-        {
-            this.OnResize(EventArgs.Empty);
-        }
+            this.panel1 = new Paway.Forms.TControl();
+            this.SuspendLayout();
+            // 
+            // lbTitle
+            // 
+            this.lbTitle.AutoSize = true;
+            this.lbTitle.BackColor = System.Drawing.Color.Transparent;
+            this.lbTitle.Dock = System.Windows.Forms.DockStyle.None;
+            this.lbTitle.Location = new System.Drawing.Point(197, 32);
+            this.lbTitle.Size = new System.Drawing.Size(51, 27);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(1, 80);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(441, 218);
+            this.panel1.TabIndex = 5;
+            // 
+            // TBaseForm2
+            // 
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.ClientSize = new System.Drawing.Size(443, 299);
+            this.Controls.Add(this.panel1);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Name = "TBaseForm2";
+            this.Padding = new System.Windows.Forms.Padding(1, 80, 1, 1);
+            this.TBrush.ColorDown = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(57)))), ((int)(((byte)(50)))));
+            this.TBrush.ColorMove = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(112)))), ((int)(((byte)(99)))));
+            this.TBrush.ColorNormal = System.Drawing.Color.FromArgb(((int)(((byte)(121)))), ((int)(((byte)(84)))), ((int)(((byte)(74)))));
+            this.TBrush.ColorSpace = System.Drawing.Color.Ivory;
+            this.Controls.SetChildIndex(this.lbTitle, 0);
+            this.Controls.SetChildIndex(this.panel1, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
-        #endregion
-
-        #region 按键事件
-        /// <summary>
-        /// 按键响应
-        /// </summary>
-        protected virtual void OnKeyDown(Keys key) { }
-        /// <summary>
-        /// 按键
-        /// </summary>
-        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
-        {
-            switch (keyData)
-            {
-                case Keys.Tab:
-                    Control current = CurrentPoint(this);
-                    Control result = NextControl(current.Location, current.Parent);
-                    if (result == null)
-                    {
-                        result = NextControl(Point.Empty, current.Parent);
-                    }
-                    if (result != null)
-                    {
-                        result.Focus();
-                    }
-                    return true;
-                case Keys.Enter | Keys.Control:
-                    ToolOk_ItemClick(this, EventArgs.Empty);
-                    break;
-                case Keys.Enter:
-                    if (IEnter) EnterFoucs();
-                    break;
-            }
-            switch (keyData)
-            {
-                case Keys.Escape:
-                    if (iExit)
-                    {
-                        ToolCancel_ItemClick(this, EventArgs.Empty);
-                    }
-                    iExit = true;
-                    break;
-                default:
-                    iExit = false;
-                    OnKeyDown(keyData);
-                    break;
-            }
-            return base.ProcessCmdKey(ref msg, keyData);
         }
-        private void EnterFoucs()
-        {
-            Control current = CurrentPoint(this);
-            if (current is QQTextBox && (current as QQTextBox).Edit.Multiline) return;
-            Control result = NextControl(current.Location, current.Parent);
-            if (result != null)
-            {
-                result.Focus();
-            }
-            else if (IEnterClick)
-            {
-                this.Focus();
-                ToolOk_ItemClick(null, EventArgs.Empty);
-            }
-        }
-
-        #endregion
-
-        #region 按钮事件
-        /// <summary>
-        /// 按钮响应
-        /// </summary>
-        protected virtual void OnItemClick(ToolItem item) { }
-        /// <summary>
-        /// 确认响应
-        /// </summary>
-        protected virtual bool OnCommit() { return true; }
-        /// <summary>
-        /// 异常
-        /// </summary>
-        protected virtual void OnFailed() { }
-        /// <summary>
-        /// 取消响应
-        /// </summary>
-        protected virtual bool OnCancel() { return true; }
-        /// <summary>
-        /// 其它响应
-        /// </summary>
-        protected void ToolBar1_ItemClick(object sender, EventArgs e)
-        {
-            try
-            {
-                ToolItem item = sender as ToolItem;
-                OnItemClick(item);
-            }
-            catch (Exception ex)
-            {
-                ExceptionHelper.Show(this, ex);
-            }
-        }
-        /// <summary>
-        /// 确认OK
-        /// </summary>
-        protected void ToolOk_ItemClick(object sender, EventArgs e)
-        {
-            try
-            {
-                if (this.DialogResult == DialogResult.OK) return;
-                if (OnCommit())
-                {
-                    this.DialogResult = DialogResult.OK;
-                    this.Close();
-                }
-            }
-            catch (Exception ex)
-            {
-                OnFailed();
-                ExceptionHelper.Show(this, ex);
-            }
-        }
-        /// <summary>
-        /// 取消Cancel
-        /// </summary>
-        protected void ToolCancel_ItemClick(object sender, EventArgs e)
-        {
-            try
-            {
-                if (this.DialogResult == DialogResult.Cancel) return;
-                if (OnCancel())
-                {
-                    this.DialogResult = DialogResult.Cancel;
-                    this.Close();
-                }
-            }
-            catch (Exception ex)
-            {
-                ExceptionHelper.Show(this, ex);
-            }
-        }
-
-        #endregion
     }
 }
